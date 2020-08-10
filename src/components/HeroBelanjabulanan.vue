8732532416e9aa2a2ec488d878aa758b756e0151
eko-prog-code <ekosetiaji929@gmail.com>
2020-08-10 11:55:54 +0700

    1st

---
+++ src/components/HeroBelanjabulanan.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <!-- Hero Section Begin -->
+    <section class="hero-section">
+      <carousel class="hero-items" :items="1" :nav="false" :autoplay="true">
+        <div
+          class="single-hero-items set-bg"
+          style="background-image: url('/img/hero-1.jpg')"
+        >
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-5">
+                <span>Belanja Bulanan</span>
+                <h1>E-Commerce Pertama di Kota Karawang</h1>
+                <p>
+                  Mengantar Kebutuhan Dapur ke Rumah Anda
+                </p>
+                <a href="#" class="primary-btn">Shop Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="single-hero-items set-bg"
+          style="background-image: url('/img/hero-2.jpg')"
+        >
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-5">
+                <span>Belanja Bulanan</span>
+                <h1>Diskon terbaik Menanti Anda</h1>
+                <p>
+                  Menyediakan Produk dengan Harga Terjangkau
+                </p>
+                <a href="#" class="primary-btn">Shop Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="single-hero-items set-bg"
+          style="background-image: url('/img/hero-3.jpg')"
+        >
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-5">
+                <span>Belanja Bulanan</span>
+                <h1>Memberikan Kemudahan dalam Berbelanja</h1>
+                <p>
+                  Motto: Sekali Sentuh Seribu Kemudahan (Digital Smart Shopping)
+                </p>
+                <a href="#" class="primary-btn">Shop Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </carousel>
+    </section>
+    <!-- Hero Section End -->
+  </div>
+</template>
+
+<script>
+import carousel from "vue-owl-carousel";
+
+export default {
+  name: "HeroBelanjabulanan",
+  components: {
+    carousel,
+  },
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderBelanjabulanan />
+    <HeroBelanjabulanan />
+    <Barang />
+    <Insta />
+    <Partner />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderBelanjabulanan from '@/components/HeaderBelanjabulanan.vue'
+import HeroBelanjabulanan from '@/components/HeroBelanjabulanan.vue'
+import Barang from '@/components/Barang.vue'
+import Insta from '@/components/Insta.vue'
+import Partner from '@/components/Partner.vue'
+import Footer from '@/components/Footer.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+HeaderBelanjabulanan,
+HeroBelanjabulanan,
+Barang, 
+Insta,
+Partner,
+Footer
+  }
+}
+</script>
